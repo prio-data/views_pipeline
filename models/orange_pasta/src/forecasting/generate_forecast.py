379ce5f01a26a76f98c63d91c294ef49f0caaa42
xiaolongsun <95378566+xiaolong0728@users.noreply.github.com>
2024-02-13 22:26:51 +0100

    Add two production models

---
+++ models/orange_pasta/src/forecasting/generate_forecast.py
@@ -0,0 +1,20 @@
+import os
+import warnings
+
+warnings.filterwarnings("ignore")
+os.environ['WANDB_SILENT'] = 'true'
+
+from views_forecasts.extensions import *
+
+
+def forecast(config):
+    print('Predicting...')
+    
+    try:
+        predictions_calib = pd.DataFrame.forecasts.read_store(name=config["storage_name"])
+    except:
+        predictions_calib = config["RunResult"].run.predict("calib", "predict", config["RunResult"].data)
+        # predictions_calib.forecasts.set_run(run_id)
+        predictions_calib.forecasts.to_store(name=config["storage_name"])
+
+    
---
+++ models/orange_pasta/src/training/train_model.py
@@ -0,0 +1,54 @@
+import os
+import wandb
+from pathlib import Path
+import warnings
+
+warnings.filterwarnings("ignore")
+os.environ['WANDB_SILENT'] = 'true'
+
+from lightgbm import LGBMRegressor
+
+from views_runs import storage, StepshiftedModels
+from views_partitioning.data_partitioner import DataPartitioner
+from views_runs.run_result import RunResult
+from views_forecasts.extensions import *
+
+from ..dataloaders.get_data import *
+
+
+def train(common_config, para_config):
+    modelstore = storage.Storage()
+    suffix = ""
+    for key, value in para_config.items():
+        suffix += f"_{key}_{value}"
+    model = globals()[common_config['algorithm']](**para_config)
+
+    # Training
+    common_config["RunResult"] = RunResult.retrain_or_retrieve(
+        retrain=common_config["force_retrain"],
+        store=modelstore,
+        partitioner=DataPartitioner({"calib": common_config["calib_partitioner_dict"]}),
+        stepshifted_models=StepshiftedModels(model, common_config["steps"], common_config['depvar']),
+        dataset=get_data(),
+        queryset_name=common_config['queryset'],
+        partition_name="calib",
+        timespan_name="train",
+        storage_name=common_config['name'] + '_calib' + suffix,
+        author_name="xiaolong",
+    )
+    common_config["storage_name"] = common_config['name'] + '_calib' + suffix
+
+    
+
+    
+
+
+
+    
+
+    
+    
+    
+    
+
+
