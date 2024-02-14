f615bf3a299cec6e0d4bbe03fce0c29a9f8c5945
nskazmi <142994595+nskazmi@users.noreply.github.com>
2024-02-14 12:08:21 +0100

    a full run from training, forecasting, ensembling and logging of mse and maps

--- run_all_models.py
+++ run_all_models.py
@@ -2,13 +2,15 @@
 from models.blue_sea import main as all_zero_baseline_model
 from models.yellow_duck import main as no_change_baseline_model
 from models.green_oracle import main_training as a_pgm_model
-
+from ensembles.red_panda.src.online_evaluation.evaluate_forecast import ensemble_mean
+from ensembles.pink_elephant.src.online_evaluation.evaluate_forecast import ensemble_median
 
 def main():
     all_zero_baseline_model.main()
     no_change_baseline_model.main()
     a_pgm_model.main()
-
+    ensemble_mean()
+    ensemble_median()
 
 if __name__ == "__main__":
     main()
