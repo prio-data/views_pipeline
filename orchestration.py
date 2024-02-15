57724d120fba4d3a91b064d7df47b397f422fc43
xiaolongsun <95378566+xiaolong0728@users.noreply.github.com>
2024-02-15 21:10:02 +0100

    orchestration script with prefect

--- orchestration.py
+++ orchestration.py
@@ -2,22 +2,25 @@ from pathlib import Path
 from prefect import flow, task
 import subprocess
 
-root_dir = Path(__file__).parent/ "models"
-
+model_dir = Path(__file__).parent/ "models"
+ensemble_dir = Path(__file__).parent/ "ensembles"
 
 # Define paths to main.py files for each model
-MODEL_MAIN_FILES = list(root_dir.rglob('main.py'))
+MODEL_MAIN_FILES = list(model_dir.rglob('main.py')) + list(ensemble_dir.rglob('main.py'))
+
+def generate_run_name(main_file):
+    return main_file.stem
 
 # Prefect task to execute a main.py file
-@task
-def execute_main(main_file):
+@task(task_run_name="{name}")
+def execute_main(main_file, name):
     subprocess.run(["python", str(main_file)])
 
 # Define Prefect Flow
 @flow(log_prints=True)
 def model_execution_flow():
-    MODEL_MAIN_FILES = list(root_dir.rglob('main.py'))
-    tasks = [execute_main(main_file) for main_file in MODEL_MAIN_FILES]
+    
+    tasks = [execute_main(main_file, main_file.parent.name) for main_file in MODEL_MAIN_FILES]
 
 # Execute Prefect workflow
 model_execution_flow()
