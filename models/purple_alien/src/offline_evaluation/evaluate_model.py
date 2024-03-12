02dcdbf4ffd4a42a036dc1caf53ccf91f929ead7
Polichinl <simmaa@prio.org>
2024-03-12 22:45:13 +0100

    new path setup to eval_model

--- models/purple_alien/src/offline_evaluation/evaluate_model.py
+++ models/purple_alien/src/offline_evaluation/evaluate_model.py
@@ -1,9 +1,6 @@
 import numpy as np
 import pickle
 import time
-import sys
-import os
-import functools
 
 import torch
 import torch.nn as nn
@@ -18,38 +15,18 @@ from sklearn.metrics import brier_score_loss
 
 import wandb
 
-
-# ------------------------- CHANGE TO A COMMON UTIL ---------------------------------
+import sys
 from pathlib import Path
 
-def setup_project_paths():
-    root_path = Path(__file__).resolve().parents[3]
+PATH = Path(__file__)
+sys.path.insert(0, str(Path(*[i for i in PATH.parts[:PATH.parts.index("views_pipeline")+1]]) / "common_utils")) # PATH_COMMON_UTILS  
+from set_path import setup_project_paths
+setup_project_paths(PATH)
 
-    # Define common paths
-    common_utils_path = root_path / "common_utils"
-    common_configs_path = root_path / "common_configs"
-
-    # Define model-specific paths
-    model_path = Path(__file__).resolve().parents[1]
-    configs_path = model_path / "configs"
-    src_path = model_path / "src"
-    utils_path = src_path / "utils"
-    architectures_path = src_path / "architectures"
-
-    paths_to_add = [common_utils_path, common_configs_path, configs_path, utils_path, architectures_path]
-
-    for path in paths_to_add:
-        path_str = str(path)
-        if path.exists() and path_str not in sys.path:
-            sys.path.insert(0, path_str)
-
-# Call the function to setup the project paths
-setup_project_paths()
-# -----------------------------------------------------------------------------------
-
+from config_hyperparameters import get_hp_config
 
 from utils import choose_model, choose_loss, choose_sheduler, get_train_tensors, get_test_tensor, apply_dropout, execute_freeze_h_option, get_log_dict, train_log, init_weights, get_data
-from config_sweep import get_swep_config
+#from config_sweep import get_swep_config
 from config_hyperparameters import get_hp_config
 
 
