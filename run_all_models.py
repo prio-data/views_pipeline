80b608723789f78fb6125fd34019397cc9069fb1
nskazmi <142994595+nskazmi@users.noreply.github.com>
2024-03-19 15:16:10 +0100

    removed green_oracle

--- run_all_models.py
+++ run_all_models.py
@@ -1,13 +1,11 @@
 
 from models.blue_sea import main as all_zero_baseline_model
 from models.yellow_duck import main as no_change_baseline_model
-from models.green_oracle import main_training as a_pgm_model
 
 
 def main():
     all_zero_baseline_model.main()
     no_change_baseline_model.main()
-    a_pgm_model.main()
 
 
 if __name__ == "__main__":
