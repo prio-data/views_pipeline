5befddd08328b714d23685c635e050377c2797a3
nskazmi <142994595+nskazmi@users.noreply.github.com>
2024-03-19 15:34:33 +0100

    stepshifting of forecasts

--- models/yellow_duck/src/forecasting/true_future_36m.py
+++ models/yellow_duck/src/forecasting/true_future_36m.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 def forecast(data_for_forecasting) -> pd.DataFrame:
     for i in range(1, 37):
-        data_for_forecasting[f"step_pred_{i}"] = data_for_forecasting['ln_ged_sb_dep']
+        data_for_forecasting[f"step_pred_{i}"] = data_for_forecasting['ln_ged_sb_dep'].shift(i).fillna(0)   
     print(data_for_forecasting)
     data_for_forecasting.to_parquet(
         f"{Path(__file__).parent.parent.parent}/data/generated/forecasts.parquet")
