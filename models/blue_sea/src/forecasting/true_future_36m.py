9a43a78567d06ab68d89d9941a5e860a6a305b53
nskazmi <142994595+nskazmi@users.noreply.github.com>
2024-03-19 15:17:03 +0100

    added mean of actuals and removed zeros

--- models/blue_sea/src/forecasting/true_future_36m.py
+++ models/blue_sea/src/forecasting/true_future_36m.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from pathlib import Path
+import numpy as np
 from ..dataloaders.queryset import get_querysets
 
 def forecast(data_for_forecasting) -> pd.DataFrame:
     for i in range(1, 37):
-        data_for_forecasting[f"step_pred_{i}"] = 0
+        data_for_forecasting[f"step_pred_{i}"] = np.mean(data_for_forecasting['ln_ged_sb_dep'])
     print(data_for_forecasting)
     data_for_forecasting.to_parquet(
         f"{Path(__file__).parent.parent.parent}/data/generated/forecasts.parquet")
