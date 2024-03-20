59da28640312185dcd66e9b80f4862a47885c869
nskazmi <142994595+nskazmi@users.noreply.github.com>
2024-03-20 13:11:52 +0100

    changed name of zero baseline variable

--- run_all_models.py
+++ run_all_models.py
@@ -1,10 +1,10 @@
 
-from models.blue_sea import main as all_zero_baseline_model
+from models.blue_sea import main as zero_baseline_model
 from models.yellow_duck import main as no_change_baseline_model
 
 
 def main():
-    all_zero_baseline_model.main()
+    zero_baseline_model.main()
     no_change_baseline_model.main()
 
 
