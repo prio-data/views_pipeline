c3e9ee8ea180d51ed6cd3ece4ae0d6e1b81e15e3
nskazmi <142994595+nskazmi@users.noreply.github.com>
2024-03-22 13:23:31 +0100

    added loading message

--- run_all_models.py
+++ run_all_models.py
@@ -7,9 +7,12 @@ def main():
         while True:
             subprocess.run(["python3", "models/blue_sea/main.py"])
             subprocess.run(["python3", "models/yellow_duck/main.py"])
-            time.sleep(2000)
+            for _ in range(20):
+                print(f'waiting for next run - time remaining: {20 - _} seconds . . . \n', end='', flush=True)
+                time.sleep(1)
+            print()  # Move to the next line before the next iteration
     except KeyboardInterrupt:
-        print("Interrupted by user. Stopping...")
+        print("\nInterrupted by user. Stopping...")
 
 
 if __name__ == "__main__":
