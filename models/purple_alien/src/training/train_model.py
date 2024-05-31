496351ae29de1af0a8806e8588956b21554ace81
Polichinl <simmaa@prio.org>
2024-05-31 14:39:22 +0200

    added handle_training.py

--- models/purple_alien/src/training/train_model.py
+++ models/purple_alien/src/training/train_model.py
@@ -3,7 +3,7 @@ import pickle
 import time
 import os
 import functools
-
+from datetime import datetime
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -143,64 +143,27 @@ def training_loop(config, model, criterion, optimizer, scheduler, views_vol, dev
 
     print('training done...')
 
-# MOVE TO NEW main.py IN  purple_alien root.  
-# def model_pipeline(config = None, project = None):
-# 
-#     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-#     print(device)
-# 
-#     # tell wandb to get started
-#     with wandb.init(project=project, entity="nornir", config=config): # project and config ignored when runnig a sweep
-# 
-#         wandb.define_metric("monthly/out_sample_month")
-#         wandb.define_metric("monthly/*", step_metric="monthly/out_sample_month")
-# 
-#         # access all HPs through wandb.config, so logging matches execution!
-#         config = wandb.config
-# 
-#         views_vol = get_data(config)
-# 
-#         # make the model, data, and optimization problem
-#         model, criterion, optimizer, scheduler = make(config, device)
-# 
-#         training_loop(config, model, criterion, optimizer, scheduler, views_vol, device)
-#         print('Done training')
-# 
-#         return(model)
-# 
-# 
-# if __name__ == "__main__":
-# 
-#     wandb.login()
-# 
-#     # model type is still a vary bad name here - it should be something like run_type... Change later!
-#     model_type_dict = {'a' : 'calibration', 'b' : 'testing', 'c' : 'forecasting'}
-#     model_type = model_type_dict[input("a) Calibration\nb) Testing\nc) Forecasting\n")]
-#     print(f'Run type: {model_type}\n')
-# 
-#     project = f"imp_new_structure_{model_type}" # temp. also a bad name. Change later!
-# 
-#     hyperparameters = get_hp_config()
-# 
-#     hyperparameters['model_type'] = model_type # bad name... ! Change later!
-#     hyperparameters['sweep'] = False
-# 
-#     start_t = time.time()
-# 
-#     model = model_pipeline(config = hyperparameters, project = project)
-# 
-#     PATH_ARTIFACTS = setup_artifacts_paths(PATH)
-# 
-#     # create the artifacts folder if it does not exist
-#     os.makedirs(PATH_ARTIFACTS, exist_ok=True)
-# 
-#     # save the model
-#     PATH_MODEL_ARTIFACT = os.path.join(PATH_ARTIFACTS, f"{model_type}_model.pt")
-#     torch.save(model, PATH_MODEL_ARTIFACT)
-# 
-#     print(f"Model saved as: {PATH_MODEL_ARTIFACT}")
-#     
-#     end_t = time.time()
-#     minutes = (end_t - start_t)/60
-#     print(f'Done. Runtime: {minutes:.3f} minutes')
-# 
+
+def handle_training(config, device, views_vol, PATH_ARTIFACTS):
+    
+    # Create the model, criterion, optimizer and scheduler
+    model, criterion, optimizer, scheduler = make(config, device)
+    
+    # Train the model
+    training_loop(config, model, criterion, optimizer, scheduler, views_vol, device)
+    print('Done training')
+
+    # just in case the artifacts folder does not exist
+    os.makedirs(PATH_ARTIFACTS, exist_ok=True)
+
+    # Define the path for the artifacts with a timestamp and a run type
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    model_filename = f"{config.run_type}_model_{timestamp}.pt"
+    PATH_MODEL_ARTIFACT = os.path.join(PATH_ARTIFACTS, model_filename)
+    
+    # save the model
+    torch.save(model, PATH_MODEL_ARTIFACT)
+    
+    # done
+    print(f"Model saved as: {PATH_MODEL_ARTIFACT}")
+
