d58701a7376c197daf18bb3aaf3b33555a976b83
Polichinl <simmaa@prio.org>
2024-06-10 23:55:08 +0200

    os -> pathlib

--- common_utils/artifacts_utils.py
+++ common_utils/artifacts_utils.py
@@ -1,9 +1,9 @@
 import os
+from pathlib import Path
 
-
-def get_model_files(path, run_type):
+def get_artifact_files(PATH, run_type):
     """
-    Retrieve model files from a directory that match the given run type and common extensions.
+    Retrieve artifact files from a directory that match the given run type and common extensions.
 
     Args:
         path (str): The directory path where model files are stored.
@@ -16,14 +16,18 @@ def get_model_files(path, run_type):
     common_extensions = ['.pt', '.pth', '.h5', '.hdf5', '.pkl', '.json', '.bst', '.txt', '.bin', '.cbm', '.onnx']
 
     # Retrieve files that start with run_type and end with any of the common extensions
-    model_files = [f for f in os.listdir(path) if f.startswith(f"{run_type}_model_") and any(f.endswith(ext) for ext in common_extensions)]
+    # artifact_files = [f for f in os.listdir(PATH) if f.startswith(f"{run_type}_model_") and any(f.endswith(ext) for ext in common_extensions)]
     
-    return model_files
+    # pathlib alternative
+    artifact_files = [f for f in PATH.iterdir() if f.is_file() and f.stem.startswith(f"{run_type}_model_") and f.suffix in common_extensions]
 
 
-def get_latest_model_artifact(path, run_type):
+    return artifact_files
+
+
+def get_latest_model_artifact(PATH, run_type):
     """
-    Retrieve the latest model artifact for a given run type based on the modification time.
+    Retrieve the path (pathlib path object) latest model artifact for a given run type based on the modification time.
 
     Args:
         path (str): The model specifc directory path where artifacts are stored. 
@@ -33,17 +37,17 @@ def get_latest_model_artifact(path, run_type):
         run_type (str): The type of run (e.g., calibration, testing, forecasting).
 
     Returns:
-        str: The path to the latest model artifact given the run type.
+        The path (pathlib path objsect) to the latest model artifact given the run type.
 
     Raises:
         FileNotFoundError: If no model artifacts are found for the given run type.
     """
 
     # List all model files for the given specific run_type with the expected filename pattern
-    model_files = get_model_files(path, run_type) #[f for f in os.listdir(path) if f.startswith(f"{run_type}_model_") and f.endswith('.pt')]
+    model_files = get_artifact_files(PATH, run_type) #[f for f in os.listdir(path) if f.startswith(f"{run_type}_model_") and f.endswith('.pt')]
     
     if not model_files:
-        raise FileNotFoundError(f"No model artifacts found for run type '{run_type}' in path '{path}'")
+        raise FileNotFoundError(f"No model artifacts found for run type '{run_type}' in path '{PATH}'")
     
     # Sort the files based on the timestamp embedded in the filename. With format %Y%m%d_%H%M%S For example, '20210831_123456.pt'
     model_files.sort(reverse=True)
@@ -53,7 +57,12 @@ def get_latest_model_artifact(path, run_type):
     print(f"artifact used: {model_files[0]}")
     
     # Return the latest model file
-    return os.path.join(path, model_files[0])
+    #PATH_MODEL_ARTIFACT = os.path.join(path, model_files[0])
+
+    # pathlib alternative
+    PATH_MODEL_ARTIFACT = Path(PATH) / model_files[0]
+
+    return PATH_MODEL_ARTIFACT
 
     # notes on stepshifted models:
     # There will be some thinking here in regards to how we store, denote (naming convention), and retrieve the model artifacts from stepshifted models.
