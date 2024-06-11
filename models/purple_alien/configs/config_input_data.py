481a1c417af7a0f625a0a65cb64bdb19ba7be6ae
Polichinl <simmaa@prio.org>
2024-06-11 22:53:39 +0200

    naive first viewers 6 test...

--- models/purple_alien/configs/config_input_data.py
+++ models/purple_alien/configs/config_input_data.py
@@ -11,14 +11,15 @@ def get_input_data_config():
     queryset_base (Queryset): A queryset containing the base data for the model training.
     """
 
+    # VIEWSER 6
     queryset_base = (Queryset("purple_alien", "priogrid_month")
-        .with_column(Column("ln_sb_best", from_table = "ged2_pgm", from_column = "ged_sb_best_count_nokgi").transform.ops.ln().transform.missing.replace_na())
-        .with_column(Column("ln_ns_best", from_table = "ged2_pgm", from_column = "ged_ns_best_count_nokgi").transform.ops.ln().transform.missing.replace_na())
-        .with_column(Column("ln_os_best", from_table = "ged2_pgm", from_column = "ged_os_best_count_nokgi").transform.ops.ln().transform.missing.replace_na())
-        .with_column(Column("month", from_table = "month", from_column = "month"))
-        .with_column(Column("year_id", from_table = "country_year", from_column = "year_id"))
-        .with_column(Column("c_id", from_table = "country_year", from_column = "country_id"))
-        .with_column(Column("col", from_table = "priogrid", from_column = "col"))
-        .with_column(Column("row", from_table = "priogrid", from_column = "row")))
+        .with_column(Column("ln_sb_best", from_lao = "ged2_pgm", from_column = "ged_sb_best_count_nokgi").transform.ops.ln().transform.missing.replace_na())
+        .with_column(Column("ln_ns_best", from_lao = "ged2_pgm", from_column = "ged_ns_best_count_nokgi").transform.ops.ln().transform.missing.replace_na())
+        .with_column(Column("ln_os_best", from_lao = "ged2_pgm", from_column = "ged_os_best_count_nokgi").transform.ops.ln().transform.missing.replace_na())
+        .with_column(Column("month", from_lao = "month", from_column = "month"))
+        .with_column(Column("year_id", from_lao = "country_year", from_column = "year_id"))
+        .with_column(Column("c_id", from_lao = "country_year", from_column = "country_id"))
+        .with_column(Column("col", from_lao = "priogrid", from_column = "col"))
+        .with_column(Column("row", from_lao = "priogrid", from_column = "row")))
 
     return queryset_base
