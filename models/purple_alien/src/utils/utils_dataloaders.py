5e44a4eb7373a4de2c0e4864df02b78e59296949
Polichinl <simmaa@prio.org>
2024-06-16 13:45:20 +0200

    tryna to get utils_dataloader to be generel..

--- models/purple_alien/src/utils/utils_dataloaders.py
+++ models/purple_alien/src/utils/utils_dataloaders.py
@@ -1,17 +1,17 @@
 # Use viewser env
 
-import sys
-from pathlib import Path
+#import sys
+#from pathlib import Path
 import argparse
 
-PATH = Path(__file__)
-sys.path.insert(0, str(Path(*[i for i in PATH.parts[:PATH.parts.index("views_pipeline")+1]]) / "common_utils")) # PATH_COMMON_UTILS  
-from set_path import setup_project_paths, setup_data_paths
-setup_project_paths(PATH)
-
-from viewser import Queryset, Column
-from ingester3.ViewsMonth import ViewsMonth
-
+#PATH = Path(__file__)
+#sys.path.insert(0, str(Path(*[i for i in PATH.parts[:PATH.parts.index("views_pipeline")+1]]) / "common_utils")) # PATH_COMMON_UTILS  
+#from set_path import setup_project_paths, setup_data_paths
+#setup_project_paths(PATH)
+#
+#from viewser import Queryset, Column
+#from ingester3.ViewsMonth import ViewsMonth
+#
 import os
 import numpy as np
 import pandas as pd
@@ -32,7 +32,7 @@ def fetch_data_from_viewser():
         pd.DataFrame: The prepared DataFrame with initial processing done.
     """
     print('Beginning file download through viewser...')
-    queryset_base = get_input_data_config()
+    queryset_base = get_input_data_config() # just used here.. 
     df = queryset_base.publish().fetch()
     df.reset_index(inplace=True)
     df.rename(columns={'priogrid_gid': 'pg_id'}, inplace=True) # arguably HydraNet or at lest vol specific
@@ -82,23 +82,23 @@ def filter_dataframe_by_month_range(df, month_first, month_last):
     return df[df['month_id'].isin(month_range)].copy()
 
 
-
-def calculate_absolute_indices(df, month_first): # arguably HydraNet or at lest vol specific
-    """
-    Calculates absolute row, column, and month indices for the DataFrame.
-
-    Args:
-        df (pd.DataFrame): The DataFrame to update.
-        month_first (int): The first month ID in the month range.
-
-    Returns:
-        pd.DataFrame: The updated DataFrame with absolute indices.
-    """
-    df['abs_row'] = df['row'] - df['row'].min()         
-    df['abs_col'] = df['col'] - df['col'].min()
-    df['abs_month'] = df['month_id'] - month_first
-    return df
-
+# moved to utils_df_to_vol_conversion.py as that is where it is used..
+#def calculate_absolute_indices(df, month_first): # arguably HydraNet or at lest vol specific
+#    """
+#    Calculates absolute row, column, and month indices for the DataFrame.
+#
+#    Args:
+#        df (pd.DataFrame): The DataFrame to update.
+#        month_first (int): The first month ID in the month range.
+#
+#    Returns:
+#        pd.DataFrame: The updated DataFrame with absolute indices.
+#    """
+#    df['abs_row'] = df['row'] - df['row'].min()         
+#    df['abs_col'] = df['col'] - df['col'].min()
+#    df['abs_month'] = df['month_id'] - month_first
+#    return df
+#
 
 def get_views_df(partition):
     """
@@ -127,7 +127,7 @@ def get_views_df(partition):
     Raises:
         ValueError: If `partition` is not one of 'calibration', 'testing', or 'forecasting'.
     """
-    df = fetch_data_from_viewser()
+    df = fetch_data_from_viewser() # then it is used here..... 
     month_first, month_last = get_month_range(partition)
     df = filter_dataframe_by_month_range(df, month_first, month_last)
     #df = calculate_absolute_indices(df, month_first) # could go in the volume creation function...
@@ -162,7 +162,7 @@ def process_partition_data(partition, PATH):
         df = pd.read_pickle(path_viewser_data)
     else:
         print('Downloading file...')
-        df = get_views_df(partition)
+        df = get_views_df(partition) # which is then used here 
         print(f'Saving file to {path_viewser_data}')
         df.to_pickle(path_viewser_data)
 
