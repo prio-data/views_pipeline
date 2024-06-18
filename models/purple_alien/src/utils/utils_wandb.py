a10908d4b530bc7ad097ee143c003bfbc72ad960
Polichinl <simmaa@prio.org>
2024-06-18 23:56:55 +0200

    doc strings

--- models/purple_alien/src/utils/utils_wandb.py
+++ models/purple_alien/src/utils/utils_wandb.py
@@ -5,34 +5,91 @@ import wandb
 # there are things in other utils that should be here...
 
 def add_wandb_monthly_metrics():
+
+    """
+    Defines the WandB metrics for monthly evaluation.
+
+    This function sets up the metrics for logging monthly evaluation metrics in WandB.
+    It defines a step metric called "monthly/out_sample_month" and specifies that any 
+    metric under the "monthly" namespace will use "monthly/out_sample_month" as its step metric.
+
+    Usage:
+        This function should be called at the start of a WandB run to configure 
+        how metrics are tracked over time steps.
+
+    Example:
+        >>> wandb.init(project="example_project")
+        >>> add_wandb_monthly_metrics()
+        >>> wandb.log({"monthly/mean_squared_error": 0.02, "monthly/out_sample_month": 1})
+
+    Notes:
+        - The step metric "monthly/out_sample_month" will be used to log metrics for each time  (i.e. forecasted month).
+        - Any metric prefixed with "monthly/" will follow the "monthly/out_sample_month" step metric.
+
+    See Also:
+        - `wandb.define_metric`: WandB API for defining metrics and their step relationships.
+    """
         
     # Define "new" monthly metrics for WandB logging
     wandb.define_metric("monthly/out_sample_month")
     wandb.define_metric("monthly/*", step_metric="monthly/out_sample_month")
 
-# not the monthly but the mean (and they are wrong... )
-#def log_wandb_mean_metrics(config, mse_list, ap_list, auc_list, brier_list):
-#    
-#    """
-#    Logs evaluation metrics to WandB.
-#
-#    This function computes the mean of provided metrics and logs them to WandB.
-#    The metrics include mean squared error, average precision score, ROC AUC score, and Brier score loss.
-#
-#    Args:
-#        config : Configuration object containing parameters and settings.
-#        mse_list : List of monthly mean squared errors.
-#        ap_list : List of monthly average precision scores.
-#        auc_list : List of monthly ROC AUC scores.
-#        brier_list : List of monthly Brier scores.
-#
-#    """
-#
-#    wandb.log({f"{config.time_steps}month_mean_squared_error": np.mean(mse_list)})
-#    wandb.log({f"{config.time_steps}month_average_precision_score": np.mean(ap_list)})
-#    wandb.log({f"{config.time_steps}month_roc_auc_score": np.mean(auc_list)})
-#    wandb.log({f"{config.time_steps}month_brier_score_loss": np.mean(brier_list)})
-#
+
+
+def generate_wandb_log_dict(log_dict, dict_of_eval_dicts, feature, step):
+
+    """
+    Adds evaluation metrics to a WandB log dictionary for a specific feature and time step (i.e. forcasted month).
+
+    This function updates the provided log dictionary with evaluation metrics from 
+    a specified feature and step, formatted for WandB logging. It appends the metrics
+    to the log dictionary using the "monthly/{metric_name}_{feature}" format.
+
+    Args:
+        log_dict (dict): The log dictionary to be updated with new metrics.
+        dict_of_eval_dicts (Dict[str, Dict[str, EvaluationMetrics]]): A dictionary of evaluation metrics,
+            where the keys are feature identifiers and the values are dictionaries with time steps as keys 
+            and `EvaluationMetrics` instances as values.
+        feature (str): The feature for which the metrics should be logged (e.g., 'sb', 'ns', 'os').
+        step (str): The specific time step (month forecasted) for which metrics are logged (e.g., 'step01').
+
+    Returns:
+        dict: The updated log dictionary with the evaluation metrics for the specified feature and step.
+
+    Example:
+        >>> log_dict = {}
+        >>> dict_of_eval_dicts = {
+        ...     'sb': {'step01': EvaluationMetrics(MSE=0.1, AP=0.2, AUC=0.3, Brier=0.4), ...},
+        ...     'ns': {'step01': EvaluationMetrics(MSE=0.5, AP=0.6, AUC=0.7, Brier=0.8), ...},
+        ...     'os': {'step01': EvaluationMetrics(MSE=0.9, AP=1.0, AUC=1.1, Brier=1.2), ...}
+        ... }
+        >>> log_dict = generate_wandb_log_dict(log_dict, dict_of_eval_dicts, 'sb', 'step01')
+        >>> print(log_dict)
+        {
+            'monthly/MSE_sb': 0.1,
+            'monthly/AP_sb': 0.2,
+            'monthly/AUC_sb': 0.3,
+            'monthly/Brier_sb': 0.4
+        }
+
+    Notes:
+        - Only non-None values from the `EvaluationMetrics` instance are added to the log dictionary.
+        - The metrics are formatted with the "monthly/{metric_name}_{feature}" naming convention for WandB logging.
+
+    See Also:
+        - `wandb.log`: WandB API for logging metrics.
+    """
+    
+    for key, value in dict_of_eval_dicts[feature][step].__dict__.items():
+        if value is not None:
+            log_dict[f"monthly/{key}_{feature}"] = value
+
+    return log_dict 
+
+
+
+
+# ---------------- Im unsure if this is used or not... ----------------
 
 
 def log_wandb_mean_metrics(config, df_eval):
@@ -71,15 +128,6 @@ def log_wandb_mean_metrics(config, df_eval):
 
 
 
-def generate_wandb_log_dict(log_dict, dict_of_eval_dicts, feature, step):
-    
-    for key, value in dict_of_eval_dicts[feature][step].__dict__.items():
-        if value is not None:
-            log_dict[f"monthly/{key}_{feature}"] = value
-
-    return log_dict 
-
-
 # old and I think deprecated... Or is this used???
 def get_log_dict(i, mean_array, mean_class_array, std_array, std_class_array, out_of_sample_vol, config):
 
@@ -114,3 +162,29 @@ def get_log_dict(i, mean_array, mean_class_array, std_array, std_class_array, ou
         log_dict[f"monthly/brier_score_loss{j}"] = brier
 
     return log_dict
+
+
+
+# not the monthly but the mean (and they are wrong... )
+#def log_wandb_mean_metrics(config, mse_list, ap_list, auc_list, brier_list):
+#    
+#    """
+#    Logs evaluation metrics to WandB.
+#
+#    This function computes the mean of provided metrics and logs them to WandB.
+#    The metrics include mean squared error, average precision score, ROC AUC score, and Brier score loss.
+#
+#    Args:
+#        config : Configuration object containing parameters and settings.
+#        mse_list : List of monthly mean squared errors.
+#        ap_list : List of monthly average precision scores.
+#        auc_list : List of monthly ROC AUC scores.
+#        brier_list : List of monthly Brier scores.
+#
+#    """
+#
+#    wandb.log({f"{config.time_steps}month_mean_squared_error": np.mean(mse_list)})
+#    wandb.log({f"{config.time_steps}month_average_precision_score": np.mean(ap_list)})
+#    wandb.log({f"{config.time_steps}month_roc_auc_score": np.mean(auc_list)})
+#    wandb.log({f"{config.time_steps}month_brier_score_loss": np.mean(brier_list)})
+#
