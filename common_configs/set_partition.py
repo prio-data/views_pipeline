edf797dfc9216e22d99a95ba5b29571c2f576a26
Polichinl <simmaa@prio.org>
2024-07-31 12:00:07 +0200

    Suggested Rolling forecasting origin eval scheme:

--- common_configs/set_partition.py
+++ common_configs/set_partition.py
@@ -13,6 +13,7 @@ def get_partitioner_dict(partion, step=36):
 
         # test_partitioner according to Hegre et al  2021: train 121-432 = 01/01/1990-12/31/2015. val: 433-468 = 01/01/2017-31/12/2018
         partitioner_dict = {"train":(121,444),"predict":(445,492)} #!!! 444 = 2016-12-31 and not 2015-12-31 and 492 = 2020-12-31 and not 2018-12-31 (468)
+        # note that since hydranet was set to only do 36 months, the end month of the test set was not important as long as it is beyond 36 ahead. 
 
     if partion == 'forecasting':
 
@@ -32,3 +33,45 @@ def get_partitioner_dict(partion, step=36):
 #        'future_partitioner_dict': {"train": (121, 504), "predict": (529, 529)}, # NO HARD CODIGN THE FUTURE START DATE
 #        'FutureStart': 529, #Jan 24 # THIS SHOULD NOT BE HARD CODED!!!! Whatever the right partitions are for calibration and testing, the forecasting should be automatically infered from the current date.
 #    }
+
+
+
+# Suggested Rolling forecasting origin eval scheme:
+#| Evaluation | Calibration validation start | Calibration validation end | Test validation start | Test validation end |
+#|------------|------------------------------|----------------------------|-----------------------|---------------------|
+#| 0          | 432                          | 467                        | 433                   | 468                 |
+#| 1          | 433                          | 468                        | 434                   | 469                 |
+#| 2          | 434                          | 469                        | 435                   | 470                 |
+#| 3          | 435                          | 470                        | 436                   | 471                 |
+#| 4          | 436                          | 471                        | 437                   | 472                 |
+#| 5          | 437                          | 472                        | 438                   | 473                 |
+#| 6          | 438                          | 473                        | 439                   | 474                 |
+#| 7          | 439                          | 474                        | 440                   | 475                 |
+#| 8          | 440                          | 475                        | 441                   | 476                 |
+#| 9          | 441                          | 476                        | 442                   | 477                 |
+#| 10         | 442                          | 477                        | 443                   | 478                 |
+#| 11         | 443                          | 478                        | 444                   | 479                 |
+#| 12         | 444                          | 479                        | 445                   | 480                 |
+#| 13         | 445                          | 480                        | 446                   | 481                 |
+#| 14         | 446                          | 481                        | 447                   | 482                 |
+#| 15         | 447                          | 482                        | 448                   | 483                 |
+#| 16         | 448                          | 483                        | 449                   | 484                 |
+#| 17         | 449                          | 484                        | 450                   | 485                 |
+#| 18         | 450                          | 485                        | 451                   | 486                 |
+#| 19         | 451                          | 486                        | 452                   | 487                 |
+#| 20         | 452                          | 487                        | 453                   | 488                 |
+#| 21         | 453                          | 488                        | 454                   | 489                 |
+#| 22         | 454                          | 489                        | 455                   | 490                 |
+#| 23         | 455                          | 490                        | 456                   | 491                 |
+#| 24         | 456                          | 491                        | 457                   | 492                 |
+#| 25         | 457                          | 492                        | 458                   | 493                 |
+#| 26         | 458                          | 493                        | 459                   | 494                 |
+#| 27         | 459                          | 494                        | 460                   | 495                 |
+#| 28         | 460                          | 495                        | 461                   | 496                 |
+#| 29         | 461                          | 496                        | 462                   | 497                 |
+#| 30         | 462                          | 497                        | 463                   | 498                 |
+#| 31         | 463                          | 498                        | 464                   | 499                 |
+#| 32         | 464                          | 499                        | 465                   | 500                 |
+#| 33         | 465                          | 500                        | 466                   | 501                 |
+#| 34         | 466                          | 501                        | 467                   | 502                 |
+#| 35         | 467                          | 502                        | 468                   | 503                 |
