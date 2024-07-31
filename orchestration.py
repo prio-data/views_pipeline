c3d35ce778e3599bc897e9cc8d062e9da68b5c5d
xiaolongsun <95378566+xiaolong0728@users.noreply.github.com>
2024-07-31 11:39:00 +0200

    second version after pr

--- orchestration.py
+++ orchestration.py
@@ -1,22 +1,13 @@
-import sys
 from pathlib import Path
 from prefect import flow, task
 import subprocess
 
-PATH = Path(__file__)
-sys.path.insert(0, str(Path(
-    *[i for i in PATH.parts[:PATH.parts.index("views_pipeline") + 1]]) / "common_utils"))  # PATH_COMMON_UTILS
-from set_path import setup_project_paths, setup_model_paths
-setup_project_paths(PATH)
-
-
 def initialize():
-    model_dir = setup_model_paths(PATH)
-
     # Define paths to main.py files for each model
-    # MODEL_MAIN_FILES = list(model_dir.rglob('main.py')) + list(ensemble_dir.rglob('main.py'))
-    MODEL_MAIN_FILES = list(model_dir.rglob('main.py'))
-    return run_id, MODEL_MAIN_FILES
+    MODEL_DIR = Path(__file__).parent / "models"
+    MODEL_MAIN_FILES = list(MODEL_DIR.rglob('main.py')) 
+
+    return MODEL_MAIN_FILES
 
 
 @task(task_run_name="{name}")
