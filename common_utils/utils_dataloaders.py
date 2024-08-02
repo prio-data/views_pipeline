8c6830749989979949eb29bf8c050e975bbe6ce5
Polichinl <simmaa@prio.org>
2024-08-02 12:49:08 +0200

    temp ourcomment of inputdrift detection util Jim fix error. Also better printout of dates in fetched df

--- common_utils/utils_dataloaders.py
+++ common_utils/utils_dataloaders.py
@@ -9,9 +9,10 @@ from set_partition import get_partitioner_dict
 from config_input_data import get_input_data_config # this is model specific... this is thi issue.. .
 from common_configs import config_drift_detection
 from utils_df_to_vol_conversion import df_to_vol
+from utils_logging_and_print_outputs import print_dataset_dates
 
 
-def fetch_data_from_viewser(month_first,month_last,drift_config_dict):
+def fetch_data_from_viewser(month_first,month_last, drift_config_dict):
     """
     Fetches and prepares the initial DataFrame from viewser.
 
@@ -20,12 +21,23 @@ def fetch_data_from_viewser(month_first,month_last,drift_config_dict):
     """
     print(f'Beginning file download through viewser with month range {month_first},{month_last}')
     queryset_base = get_input_data_config() # just used here.. 
-    df, alerts = queryset_base.publish().fetch_with_drift_detection(month_first, month_last-1, drift_config_dict)
+
+# Input drift dection 
+#    df, alerts = queryset_base.publish().fetch_with_drift_detection(month_first, month_last-1, drift_config_dict)
+
+# no Input drift detection
+    df = queryset_base.publish().fetch() #.fetch_with_drift_detection(month_first, month_last-1, drift_config_dict)
+
     df.reset_index(inplace=True)
     df.rename(columns={'priogrid_gid': 'pg_id'}, inplace=True) # arguably HydraNet or at lest vol specific
     df['in_viewser'] = True  # arguably HydraNet or at lest vol specific
-    for alert in alerts:
-        print(alert)
+
+    df = filter_dataframe_by_month_range(df, month_first, month_last)
+
+# Input drift detection    
+#    for alert in alerts:
+#        print(alert)
+
     return df
 
 
@@ -72,18 +84,18 @@ def get_drift_config_dict(partition):
 
     return drift_config_dict
 
-def validate_df_partition(df,partition,override_month=None):
+def validate_df_partition(df, partition, override_month=None):
 
     """
     Checks to see if the min and max months in the input dataframe are the same as the min
-    month in the train and max month in the predict portions (or min and max months in the train portion for
+    month in the train and max month in the predict set (or min and max months in the train set for
     the forecasting partition)
 
     Args:
         df: dataframe to be checked
         partition: partition against which to check
         override_month: if user has overridden the end month of the forecasting partition, this value
-        is substituted for the last month in the forecasting train portion
+        is substituted for the last month in the forecasting train set
 
     Returns:
         True=success, False=failed
@@ -92,9 +104,16 @@ def validate_df_partition(df,partition,override_month=None):
 
     df_time_units = df['month_id'].values
     partitioner_dict = get_partitioner_dict(partition)
+    
     if partition in ['calibration', 'testing']:
         first_month = partitioner_dict['train'][0]
         last_month = partitioner_dict['predict'][1]
+
+        # debug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        #print(f'first_month: {first_month}, last_month: {last_month}')
+        #print(f'{np.min(df_time_units)}, {np.max(df_time_units)}') # okay so here is a bug - the dataframe goes from 1 to 852... 
+        # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
     else:
         first_month = partitioner_dict['train'][0]
         last_month = partitioner_dict['train'][1]
@@ -106,7 +125,7 @@ def validate_df_partition(df,partition,override_month=None):
     else:
         return True
 
-def filter_dataframe_by_month_range(df, month_first, month_last):
+def filter_dataframe_by_month_range(df, month_first, month_last): # This is currentloy used in fetch_data_from_viewser (above). But when/if we can specificy the month range in the queryset then that should be done instead.
     """
     Filters the DataFrame to include only the specified month range.
 
@@ -122,7 +141,7 @@ def filter_dataframe_by_month_range(df, month_first, month_last):
     return df[df['month_id'].isin(month_range)].copy()
 
 
-def get_views_df(partition,override_month=None):
+def get_views_df(partition, override_month=None):
     """
     Fetches and processes a DataFrame containing spatial-temporal data for the specified partition type.
     
@@ -153,10 +172,9 @@ def get_views_df(partition,override_month=None):
     month_first, month_last = get_month_range(partition)
     drift_config_dict = get_drift_config_dict(partition)
 
-    if partition == 'forecasting' and override_month is not None:
+    if partition == 'forecasting' and override_month is not None: 
         month_last = override_month
         print(f'\n ***Warning: overriding end month in forecasting partition to {month_last} ***\n')
-
 
     df = fetch_data_from_viewser(month_first, month_last, drift_config_dict)
 
@@ -181,7 +199,7 @@ def fetch_or_load_views_df(partition, PATH_RAW, use_saved=False, override_month=
         pd.DataFrame: The DataFrame fetched or loaded from viewser, with minimum preprocessing applied.
     """
 
-    path_viewser_df = os.path.join(str(PATH_RAW), f'{partition}_viewser_df.pkl') #maby change to df...
+    path_viewser_df = os.path.join(str(PATH_RAW), f'{partition}_viewser_df.pkl') #maybe change to df...
 
     # Create the folders if they don't exist
     os.makedirs(str(PATH_RAW), exist_ok=True)
@@ -202,12 +220,16 @@ def fetch_or_load_views_df(partition, PATH_RAW, use_saved=False, override_month=
         print(f'Saving file to {path_viewser_df}')
         df.to_pickle(path_viewser_df)
 
+    
+    # return df 
+    print_dataset_dates(df)
+    
     if validate_df_partition(df, partition, override_month):
-
         return df
 
     else:
-        raise RuntimeError(f'file at {path_viewser_df} incompatible with partition {partition}')
+        raise RuntimeError(f'file at {path_viewser_df} incompatible with partition {partition}') # so now we save but also stop the sctipt?
+
 
 # could be moved to common_utils/utils_df_to_vol_conversion.py but it is not really a conversion function so I would keep it here for now.
 def create_or_load_views_vol(partition, PATH_PROCESSED, PATH_RAW):
