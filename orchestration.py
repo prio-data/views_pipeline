0118b4ef6ad78728233f497a1da758f49737674e
xiaolongsun <95378566+xiaolong0728@users.noreply.github.com>
2024-08-27 14:26:19 +0200

    finish orchestrating single model

--- orchestration.py
+++ orchestration.py
@@ -1,45 +1,72 @@
 from pathlib import Path
-import sys
-
-PATH = Path(__file__)
-sys.path.insert(0, str(Path(*[i for i in PATH.parts[:PATH.parts.index("views_pipeline")]]) / "common_utils")) # PATH_COMMON_UTILS  
-from set_path import setup_project_paths, setup_model_paths
-setup_project_paths(PATH)
-#setup_model_paths(PATH)
-#model_dir = PATH_MODEL
-model_dir = Path(__file__).parent/ "models" # TBD: add PATH_MODEL to seth_path.py
-ensemble_dir = Path(__file__).parent/ "ensembles" # TBD: add PATH_ENSEMBLE to seth_path.py
-
-from config_monthly import get_monthly_config
-
 from prefect import flow, task
 import subprocess
+from common_utils.utils_cli_parser import parse_args, validate_arguments
+
+MODEL_DIR = Path(__file__).parent / "models"
+ENSEMBLE_DIR = Path(__file__).parent / "ensemble"
 
 
 def initialize():
-    #Define run id from common_configs/config_monthly.py
-    monthly_config = get_monthly_config()
-    run_id = monthly_config["run_id"]
+    # Define paths to main.py files for each model and ensemble
+    model_main_files = list(MODEL_DIR.rglob('main.py')) 
+    ensemble_main_files = list(ENSEMBLE_DIR.rglob('main.py'))
 
-    # Define paths to main.py files for each model
-    MODEL_MAIN_FILES = list(model_dir.rglob('main.py')) + list(ensemble_dir.rglob('main.py'))
-
-    return run_id, MODEL_MAIN_FILES
+    return model_main_files, ensemble_main_files
 
 
 @task(task_run_name="{name}")
-def execute_main(main_file, name):
-    # Prefect task to execute a main.py file
-    subprocess.run(["python", str(main_file)])
+def run_model_script(script_path, name, run_type, sweep, train, evaluate, forecast):
+    cli_args = []
+    cli_args.append("--run_type")
+    cli_args.append(run_type)
+
+    if sweep:
+        cli_args.append("--sweep")
+    if train:
+        cli_args.append("--train")
+    if evaluate:
+        cli_args.append("--evaluate")
+    if forecast:
+        cli_args.append("--forecast")
+
+    command = ["python", script_path] + cli_args
+    # print(command)
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode != 0:
+        raise Exception(f"Script {script_path} failed: {result.stderr}")
+    print(result.stdout)
+
+
+@task(task_run_name="{name}")
+def run_ensemble_script(script_path, name, run_type, aggregation):
+    cli_args = []
+    cli_args.extend(["--run_type", run_type])
+    cli_args.extend(["--aggregation", aggregation])
+    
+
+    command = ["python", script_path] + cli_args
+    # print(command)
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode != 0:
+        raise Exception(f"Script {script_path} failed: {result.stderr}")
+    print(result.stdout)
+
 
 @flow(log_prints=True)
-def model_execution_flow():
-    # Define Prefect Flow
-    #run_id, MODEL_MAIN_FILES = initialize()
-    tasks = [execute_main(main_file, main_file.parent.name) for main_file in MODEL_MAIN_FILES] # Execute each model in parallel
+def model_execution_flow(run_type, sweep, train, evaluate, forecast):
+    MODEL_MAIN_FILES = initialize()
+    for main_file in MODEL_MAIN_FILES:
+        run_model_script(main_file, main_file.parent.name, run_type, sweep, train, evaluate, forecast)
 
-# Execute Prefect workflow
-model_execution_flow()
 
 if __name__ == "__main__":
-    model_execution_flow.run()
+    args = parse_args()
+    validate_arguments(args)
+
+    model_execution_flow(run_type=args.run_type, 
+               sweep=args.sweep, 
+               train=args.train, 
+               evaluate=args.evaluate, 
+               forecast=args.forecast)
+
