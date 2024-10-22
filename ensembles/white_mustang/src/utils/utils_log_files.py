0af293669a7a919ebb4b3b7b89aae90e23e864ef
xiaolongsun <95378566+xiaolong0728@users.noreply.github.com>
2024-10-22 12:42:50 +0200

    More robust

--- ensembles/white_mustang/src/utils/utils_log_files.py
+++ ensembles/white_mustang/src/utils/utils_log_files.py
@@ -1,36 +1,13 @@
-from pathlib import Path
+import sys
 import logging
+from pathlib import Path
+PATH = Path(__file__)
+sys.path.insert(0, str(Path(
+    *[i for i in PATH.parts[:PATH.parts.index("views_pipeline") + 1]]) / "common_utils")) 
+
+from model_path import ModelPath 
 
 logger = logging.getLogger(__name__)
-
-
-def create_log_file(PATH_GENERATED,
-                    config,
-                    model_timestamp,
-                    data_generation_timestamp=None,
-                    data_fetch_timestamp=None):
-    """
-    Creates a log file in the specified model-specific folder with details about the generated data.
-
-    Args:
-    - PATH_GENERATED (Path): The path to the folder where the log file will be created.
-    - config (dict): The configuration dictionary containing the model details.
-    - model_timestamp (str): The timestamp when the model was trained.
-    - data_generation_timestamp (str): The timestamp when the data was generated.
-    - data_fetch_timestamp (str, optional): The timestamp when the raw data used was fetched from VIEWS.
-    """
-
-    Path(PATH_GENERATED).mkdir(parents=True, exist_ok=True)
-    log_file_path = f"{PATH_GENERATED}/{config['run_type']}_log.txt"
-
-    with open(log_file_path, 'w') as log_file:
-        log_file.write(f"Model Name: {config['name']}\n")
-        log_file.write(f"Model Timestamp: {model_timestamp}\n")
-        log_file.write(f"Data Generation Timestamp: {data_generation_timestamp}\n")
-        log_file.write(f"Data Fetch Timestamp: {data_fetch_timestamp}\n")
-        log_file.write(f"Deployment Status: {config['deployment_status']}\n")
-
-    logger.info(f"Model log file created at: {log_file_path}")
 
 
 def read_log_file(log_file_path):
@@ -44,9 +21,80 @@ def read_log_file(log_file_path):
     - dict: A dictionary containing the model name, model timestamp, data generation timestamp, and data fetch timestamp.
     """
     log_data = {}
-    with open(log_file_path, 'r') as file:
+    with open(log_file_path, "r") as file:
         for line in file:
-            key, value = line.strip().split(': ', 1)
-            log_data[key] = value
+            line = line.strip()
 
+            # Skip blank lines
+            if not line:
+                continue
+            else:
+                key, value = line.split(": ", 1)
+                # There are duplicated keys for ensemble models, but it's not a problem bc these keys are not used
+                log_data[key] = value
     return log_data
+
+
+def create_specific_log_file(PATH_GENERATED,
+                    run_type,
+                    model_name,
+                    deployment_status,
+                    model_timestamp,
+                    data_generation_timestamp=None,
+                    data_fetch_timestamp=None,
+                    model_type="single",
+                    mode="w",):
+    """
+    Creates a log file in the specified model-specific folder with details about the generated data.
+
+    Args:
+    - PATH_GENERATED (Path): The path to the folder where the log file will be created.
+    - run_type (str): The type of run
+    - model_name (str): The name of the model.
+    - deployment_status (str): The status of the deployment.
+    - model_timestamp (str): The timestamp when the model was trained.
+    - data_generation_timestamp (str): The timestamp when the data was generated.
+    - data_fetch_timestamp (str, optional): The timestamp when the raw data used was fetched from VIEWS.
+    - model_type (str, optional): The type of model. Default is "single".
+    - mode (str, optional): The mode in which the file will be opened. Default is "w".
+    """
+
+    Path(PATH_GENERATED).mkdir(parents=True, exist_ok=True)
+    log_file_path = f"{PATH_GENERATED}/{run_type}_log.txt"
+
+    # Capitalize the first letter of the model type
+    model_type = model_type[0].upper() + model_type[1:]
+
+    with open(log_file_path, mode) as log_file:
+        log_file.write(f"{model_type} Model Name: {model_name}\n")
+        log_file.write(f"{model_type} Model Timestamp: {model_timestamp}\n")
+        log_file.write(f"Data Generation Timestamp: {data_generation_timestamp}\n")
+        log_file.write(f"Data Fetch Timestamp: {data_fetch_timestamp}\n")
+        log_file.write(f"Deployment Status: {deployment_status}\n\n")
+
+
+def create_log_file(PATH_GENERATED,
+                    model_config,
+                    model_timestamp,
+                    data_generation_timestamp=None,
+                    data_fetch_timestamp=None, 
+                    model_type="single",
+                    models=None):
+    
+    run_type = model_config["run_type"]
+    model_name = model_config["name"]
+    deployment_status = model_config["deployment_status"]
+    
+    create_specific_log_file(PATH_GENERATED, run_type, model_name, deployment_status,
+                    model_timestamp, data_generation_timestamp, data_fetch_timestamp, model_type)
+    if models:
+        for m_name in models:
+            model_path = ModelPath(m_name, validate=False)
+            log_data = read_log_file(model_path.data_generated / f"{run_type}_log.txt")
+            create_specific_log_file(PATH_GENERATED, run_type, m_name, log_data["Deployment Status"], 
+                                     log_data["Single Model Timestamp"], log_data["Data Generation Timestamp"], log_data["Data Fetch Timestamp"], mode="a")
+    
+    logger.info(f"Log file created at {PATH_GENERATED}/{run_type}_log.txt")
+        
+
+
