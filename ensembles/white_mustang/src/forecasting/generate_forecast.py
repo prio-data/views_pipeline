a7e4fbc4fa83fd0e5cb8e832641b6ee03db0fc03
xiaolongsun <95378566+xiaolong0728@users.noreply.github.com>
2024-10-22 12:45:26 +0200

    save single model output

--- ensembles/white_mustang/src/forecasting/generate_forecast.py
+++ ensembles/white_mustang/src/forecasting/generate_forecast.py
@@ -14,14 +14,15 @@ setup_project_paths(PATH)
 from set_partition import get_partitioner_dict
 from utils_log_files import create_log_file
 from utils_outputs import save_predictions
-from utils_run import get_standardized_df, get_aggregated_df
+from utils_run import get_standardized_df, get_aggregated_df, get_single_model_config
 from utils_artifacts import get_latest_model_artifact
 
 logger = logging.getLogger(__name__)
 
 
 def forecast_ensemble(config):
-    run_type = config['run_type']
+    run_type = config["run_type"]
+    steps = config["steps"]
     PATH_MODELS = setup_root_paths(PATH) / "models"
     _, _, PATH_GENERATED_E = setup_data_paths(PATH)
     dfs = []
@@ -33,17 +34,21 @@ def forecast_ensemble(config):
         PATH_MODEL = PATH_MODELS / model
         PATH_RAW, _, PATH_GENERATED = setup_data_paths(PATH_MODEL)
         PATH_ARTIFACTS = setup_artifacts_paths(PATH_MODEL)
-        PATH_ARTIFACT = get_latest_model_artifact(PATH_ARTIFACTS, config.run_type)
+        PATH_ARTIFACT = get_latest_model_artifact(PATH_ARTIFACTS, run_type)
 
         ts = PATH_ARTIFACT.stem[-15:]
         timestamp += model + ts + '_'
 
-        pkl_path = f'{PATH_GENERATED}/predictions_{config.steps[-1]}_{config.run_type}_{ts}.pkl'
+        pkl_path = f'{PATH_GENERATED}/predictions_{steps[-1]}_{run_type}_{ts}.pkl'
         if Path(pkl_path).exists():
             with open(pkl_path, 'rb') as file:
                 df = pickle.load(file)
         else:
+            model_config = get_single_model_config(model)
+            model_config["timestamp"] = ts
+            model_config["run_type"] = run_type
             df_viewser = pd.read_pickle(PATH_RAW / f"{run_type}_viewser_df.pkl")
+
             try:
                 stepshift_model = pd.read_pickle(PATH_ARTIFACT)
             except FileNotFoundError:
@@ -51,13 +56,22 @@ def forecast_ensemble(config):
 
             partition = get_partitioner_dict(run_type)['predict']
             df = stepshift_model.future_point_predict(partition[0]-1, df_viewser, keep_specific=True)
-            df = get_standardized_df(df, config)
+            df = get_standardized_df(df, model_config)
+
+            data_generation_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            save_predictions(df, PATH_GENERATED, model_config)
+            create_log_file(PATH_GENERATED, model_config, ts, data_generation_timestamp)
+
         dfs.append(df)
+
     df_prediction = get_aggregated_df(dfs, config["aggregation"])
     data_generation_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
     # I don't think current timestamp is useful here.
     # Timestamp of single models is more important but how should we register them in ensemble config?
     config["timestamp"] = timestamp[:-1]
-    save_predictions(df_prediction, config, PATH_GENERATED_E)
-    create_log_file(PATH_GENERATED_E, config, data_generation_timestamp)
+    save_predictions(df_prediction, PATH_GENERATED_E, config)
+
+    # How to define an ensemble model timestamp? Currently set as data_generation_timestamp.
+    create_log_file(PATH_GENERATED_E, config, data_generation_timestamp, data_generation_timestamp, 
+                    model_type="ensemble", models=config["models"])
