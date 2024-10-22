748091272e3e51f0a1c09a97fcf2f7ba0ca98a00
xiaolongsun <95378566+xiaolong0728@users.noreply.github.com>
2024-10-22 12:43:50 +0200

    More robust

--- common_utils/utils_evaluation_metrics.py
+++ common_utils/utils_evaluation_metrics.py
@@ -135,15 +135,15 @@ def generate_metric_dict(df, config):
         There are no classes in data, so we cannot calculate roc_auc_score, ap_score
     """
 
-    evaluation_dict = EvaluationMetrics.make_evaluation_dict(steps=config.steps[-1])
-    for step in config.steps:
-        evaluation_dict[f"step{str(step).zfill(2)}"].MSE = mean_squared_error(df[config.depvar], df[f"step_pred_{step}"])
-        evaluation_dict[f"step{str(step).zfill(2)}"].MAE = mean_absolute_error(df[config.depvar], df[f"step_pred_{step}"])
-        # evaluation_dict[f"step{str(step).zfill(2)}"].MSLE = mean_squared_log_error(df[config.depvar], df[f"step_pred_{step}"])
-        evaluation_dict[f"step{str(step).zfill(2)}"].CRPS = ps.crps_ensemble(df[config.depvar], df[f"step_pred_{step}"]).mean()
-        # evaluation_dict[f"step{str(step).zfill(2)}"].Brier = brier_score_loss(df[config.depvar], df[f"step_pred_{step}"])
-        # evaluation_dict[f"step{str(step).zfill(2)}"].AUC = roc_auc_score(df[config.depvar], df[f"step_pred_{step}"])
-        # evaluation_dict[f"step{str(step).zfill(2)}"].AP = average_precision_score(df[config.depvar], df[f"step_pred_{step}"])
+    evaluation_dict = EvaluationMetrics.make_evaluation_dict(steps=config["steps"][-1])
+    for step in config["steps"]:
+        evaluation_dict[f"step{str(step).zfill(2)}"].MSE = mean_squared_error(df[config["depvar"]], df[f"step_pred_{step}"])
+        evaluation_dict[f"step{str(step).zfill(2)}"].MAE = mean_absolute_error(df[config["depvar"]], df[f"step_pred_{step}"])
+        # evaluation_dict[f"step{str(step).zfill(2)}"].MSLE = mean_squared_log_error(df[config["depvar"]], df[f"step_pred_{step}"])
+        evaluation_dict[f"step{str(step).zfill(2)}"].CRPS = ps.crps_ensemble(df[config["depvar"]], df[f"step_pred_{step}"]).mean()
+        # evaluation_dict[f"step{str(step).zfill(2)}"].Brier = brier_score_loss(df[config["depvar"]], df[f"step_pred_{step}"])
+        # evaluation_dict[f"step{str(step).zfill(2)}"].AUC = roc_auc_score(df[config["depvar"]], df[f"step_pred_{step}"])
+        # evaluation_dict[f"step{str(step).zfill(2)}"].AP = average_precision_score(df[config["depvar"]], df[f"step_pred_{step}"])
     evaluation_dict = EvaluationMetrics.output_metrics(evaluation_dict)
     df_evaluation_dict = EvaluationMetrics.evaluation_dict_to_dataframe(evaluation_dict)
     return evaluation_dict, df_evaluation_dict
