fe61e86e808b484eb9c6083ad6c2c41bfc37fb30
xiaolongsun <95378566+xiaolong0728@users.noreply.github.com>
2024-10-23 13:24:34 +0200

    improve utils_log_files

--- models/electric_relaxation/src/utils/utils_log_files.py
+++ models/electric_relaxation/src/utils/utils_log_files.py
@@ -8,7 +8,9 @@ def create_log_file(PATH_GENERATED,
                     config,
                     model_timestamp,
                     data_generation_timestamp=None,
-                    data_fetch_timestamp=None):
+                    data_fetch_timestamp=None,
+                    model_type="single",
+                    mode="w"):
     """
     Creates a log file in the specified model-specific folder with details about the generated data.
 
@@ -23,11 +25,11 @@ def create_log_file(PATH_GENERATED,
     Path(PATH_GENERATED).mkdir(parents=True, exist_ok=True)
     log_file_path = f"{PATH_GENERATED}/{config['run_type']}_log.txt"
 
-    with open(log_file_path, 'w') as log_file:
-        log_file.write(f"Model Name: {config['name']}\n")
-        log_file.write(f"Model Timestamp: {model_timestamp}\n")
+    with open(log_file_path, mode) as log_file:
+        log_file.write(f"{model_type} Model Name: {config['name']}\n")
+        log_file.write(f"{model_type} Model Timestamp: {model_timestamp}\n")
         log_file.write(f"Data Generation Timestamp: {data_generation_timestamp}\n")
         log_file.write(f"Data Fetch Timestamp: {data_fetch_timestamp}\n")
-        log_file.write(f"Deployment Status: {config['deployment_status']}\n")
+        log_file.write(f"Deployment Status: {config['deployment_status']}\n\n")
 
     logger.info(f"Model log file created at: {log_file_path}")
