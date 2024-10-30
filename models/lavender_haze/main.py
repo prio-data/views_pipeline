6ed5564ca0d1e142b353ef1569912d0d83240b7e
Dylan <52908667+smellycloud@users.noreply.github.com>
2024-10-30 12:13:22 +0100

    add modelpath and split by model support

--- models/lavender_haze/main.py
+++ models/lavender_haze/main.py
@@ -13,14 +13,18 @@ from utils_cli_parser import parse_args, validate_arguments
 from utils_logger import setup_logging
 from execute_model_runs import execute_sweep_run, execute_single_run
 
+from common_utils.model_path import ModelPath
+from common_utils.global_cache import GlobalCache
+
 warnings.filterwarnings("ignore")
 
+GlobalCache["current_model"] = ModelPath.get_model_name_from_path(Path(__file__))
 logger = setup_logging("run.log")
 
 
 if __name__ == "__main__":
     wandb.login()
-
+    
     args = parse_args()
     validate_arguments(args)
 
