ee7f9295a4b516ecec22f18803876fb8f7b25b9b
Borbála Farkas <budubudu@Borbala-MacBook-Pro.local>
2024-11-01 14:15:42 +0100

    brown_cheese initial

---
+++ models/brown_cheese/src/dataloaders/get_data.py
@@ -0,0 +1,14 @@
+import logging
+from model_path import ModelPath
+from utils_dataloaders import fetch_or_load_views_df
+
+logger = logging.getLogger(__name__)
+
+def get_data(args, model_name, self_test):
+    model_path = ModelPath(model_name, validate=False)
+    path_raw = model_path.data_raw
+
+    data, alerts = fetch_or_load_views_df(model_name, args.run_type, path_raw, self_test, use_saved=args.saved)
+    logger.debug(f"DataFrame shape: {data.shape if data is not None else 'None'}")
+
+    return data
