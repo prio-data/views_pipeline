a13b0750d88939f651320df27e0d7d7b89f3a117
marinamatic <147725905+marinamatic@users.noreply.github.com>
2024-11-04 17:14:05 +0100

    created demon_days

---
+++ models/demon_days/configs/config_meta.py
@@ -0,0 +1,18 @@
+def get_meta_config():
+    """
+    Contains the meta data for the model (model algorithm, name, target variable, and level of analysis).
+    This config is for documentation purposes only, and modifying it will not affect the model, the training, or the evaluation.
+
+    Returns:
+    - meta_config (dict): A dictionary containing model meta configuration.
+    """
+    
+    meta_config = {
+        "name": "demon_days", 
+        "algorithm": "RandomForestModel",
+        "depvar": "ln_ged_sb_dep",
+        "queryset": "fatalities002_faostat",
+        "level": "cm",
+        "creator": "Marina"
+    }
+    return meta_config
