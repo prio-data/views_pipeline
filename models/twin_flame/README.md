0bb8e4a0b433d2eb9523753e9839f6639fa95b2e
Borbála Farkas <36622811+lujzi05@users.noreply.github.com>
2024-11-05 11:38:41 +0100

    created twin_flame

---
+++ models/twin_flame/README.md
@@ -0,0 +1,3 @@
+# Model README
+## Model name: twin_flame
+## Created on: 2024-11-05 11:17:17.580234
---
+++ models/twin_flame/configs/config_meta.py
@@ -0,0 +1,20 @@
+def get_meta_config():
+    """
+    Contains the meta data for the model (model algorithm, name, target variable, and level of analysis).
+    This config is for documentation purposes only, and modifying it will not affect the model, the training, or the evaluation.
+
+    Returns:
+    - meta_config (dict): A dictionary containing model meta configuration.
+    """
+    
+    meta_config = {
+        "name": "twin_flame", 
+        "algorithm": "HurdleModel",
+        "model_clf": "LightGBMModel",
+        "model_reg": "LightGBMModel",
+        "depvar": "ln_ged_sb_dep",
+        "queryset": "fatalities002_topics",
+        "level": "cm",
+        "creator": "Borbála"
+    }
+    return meta_config
